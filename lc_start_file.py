79b38748b430a56156e8e3a108e59dbf0065e6b8
JB <me@me.com>
2023-07-12 15:35:04 -0500

    added lc start file

---
+++ lc_start_file.py
@@ -0,0 +1,41 @@
+'''List comprehensions provide a concise way to create lists. 
+
+It consists of brackets containing an expression followed by a for clause, then
+zero or more for or if clauses. The expressions can be anything, meaning you can
+put in all kinds of objects in lists.
+
+The result will be a new list resulting from evaluating the expression in the
+context of the for and if clauses which follow it. 
+
+The list comprehension always returns a result list. '''
+
+
+'''
+new_list = []
+for i in original_list:
+    if filter(i):
+        new_list.append(expressions(i))  '''
+
+#You can obtain the same thing using list comprehension:
+
+# new_list = [expression(i) for i in original_list if filter(i)]
+
+
+#The list comprehension starts with a '[' and ']', to help you remember that the
+#result is going to be a list.
+
+	
+# A short way to remember LC is:
+
+#result  = [*transform/expression*    *iteration*         *filter/condition*     ]
+
+
+
+
+
+
+
+
+
+
+
