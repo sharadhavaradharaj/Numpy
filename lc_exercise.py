48e4c2aa20db64897c2c3d480fa21fedd2ac77fd
JB <me@me.com>
2023-07-12 15:24:26 -0500

    initial commit

---
+++ lc_exercise.py
@@ -0,0 +1,22 @@
+## Exercise ##
+
+# 1 Using a list comprehension, create a new list called "newlist" out of the list "numbers", 
+# which contains only the positive numbers from the list, as integers.
+
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+
+
+
+
+## 2 create a list of integers which specify the length of each word in
+## a sentence except for the word 'the'
+sentence = "the quick brown fox jumps over the lazy dog"
+words = sentence.split()
+
+
+## 3 Given dictionary is consisted of vehicles and their weights in kilograms. 
+## Contruct a list of the names of vehicles with weight below 5000 kilograms. 
+## In the same list comprehension make the key names all upper case.
+dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, 
+"Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
+
